e74bbe96ea5d853a2ced5900b121e7fa68a74376
DanielM3388 <israelibit@gmail.com>
2019-03-28 02:02:13 +0200

    reworked Option

--- src/stv/Option.java
+++ src/stv/Option.java
@@ -4,30 +4,22 @@ public class Option {
 	private String mOption;
 	private int mPlace;
 	
-	public Option(String option, int place) {
+	public Option(String option, int place, int lastPlace) {
 		mOption = option;
+
 		try {
-			setPlace(place);
+			setPlace(place, lastPlace);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public Option(String option) {
-		mOption = option;
 	}
 	
 	public int getPlace() {
 		return mPlace;
 	}
 	
-
-	public String getOption() {
-		return mOption;
-	}
-	
-	protected void setPlace(int place) throws Exception {
-		if(place <= 0)
+	private void setPlace(int place, int lastPlace) throws Exception {
+		if(place <= 0 || place > lastPlace)
 			throw new Exception("Illegal place value");
 		mPlace = place;
 	}
